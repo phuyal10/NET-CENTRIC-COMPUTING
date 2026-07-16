18a6dcd2c2650d79320bae623df2fa15ce2883c5
Phuyalaryan <phuyalaryan@gmail.com>
2026-07-16 22:20:48 +0545

    async and await

--- Assignment-5/Async without await.cs
+++ Assignment-5/Async without await.cs
@@ -1,1 +1,17 @@
+using System;
 
+delegate void Notify(string message);
+
+class Program
+{
+    static void Main()
+    {
+        Notify target = ShowMessage;
+        target("Hello from the delegate!");
+    }
+
+    static void ShowMessage(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+}
