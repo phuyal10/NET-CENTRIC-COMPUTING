6a1a30dd0f5f1ba29e5c8d1baa4a7e286dde97a3
Phuyalaryan <phuyalaryan@gmail.com>
2026-07-16 22:15:26 +0545

    commit all

--- Assignment-4/Event.cs
+++ Assignment-4/Event.cs
@@ -1,1 +1,29 @@
+using System;
 
+class Button
+{
+    public event Action OnClick;
+
+    public void Press()
+    {
+        if (OnClick != null)
+        {
+            OnClick();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Button btn = new Button();
+        btn.OnClick += TurnOnLight;
+        btn.Press();
+    }
+
+    static void TurnOnLight()
+    {
+        Console.WriteLine("The light is now on.");
+    }
+}
