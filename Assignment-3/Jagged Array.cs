fd0a6ce7b3bdca6049f87c7d517d5f142c644201
Phuyalaryan <phuyalaryan@gmail.com>
2026-07-16 22:13:05 +0545

    jagged array

--- Assignment-3/Jagged Array.cs
+++ Assignment-3/Jagged Array.cs
@@ -1,1 +1,50 @@
+using System;
 
+class JaggedCube
+{
+    private int[][][] data;
+
+    public JaggedCube(int depth, int rows, int cols)
+    {
+        data = new int[depth][][];
+        for (int d = 0; d < depth; d++)
+        {
+            data[d] = new int[rows][];
+            for (int r = 0; r < rows; r++)
+            {
+                data[d][r] = new int[cols];
+            }
+        }
+    }
+
+    public int this[int d, int r, int c]
+    {
+        get { return data[d][r][c]; }
+        set { data[d][r][c] = value; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        JaggedCube myCube = new JaggedCube(2, 3, 4);
+
+        myCube[0, 0, 0] = 5;
+        myCube[0, 1, 2] = 10;
+        myCube[1, 2, 3] = 15;
+
+        for (int d = 0; d < 2; d++)
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    Console.Write(myCube[d, r, c] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
